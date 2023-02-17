1da0f4ecf1b591cfbc487b83647c257c526762fd
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-02-16 21:57:49 -0600

    LinkedList Implementation start

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,45 @@
 'use strict';
 
-class LinkedList {
+class Node {
+  constructor(value, next=null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  includes(value) {
+    let current = this.head;
+    if (current.value === value) return true;
+    while(current.next) {
+      if (current.next.value === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  insert(value) {
+    const oldHead = this.head;
+    this.head = new Node(value);
+    this.head.next = oldHead;
+    this.length++;
+  }
+
+  toString() {
+
+    let current = this.head;
+    let text = '';
+    while(current) {
+      text += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    return text + 'NULL';
+  }
 }
 
 module.exports = LinkedList;
